97f18021429095d748355ab00880af90c42d7871
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-07-19 15:48:54 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,16 +46,18 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
     duration: 2000,
-    reset: false
+    reset: true
 })
 
+/* SCROLL NAV*/
+sr.reveal('#navname', )
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home__img', {origin:'bottom', delay: 400})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__img', {origin:'left', delay: 500})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
@@ -66,11 +68,13 @@ sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '25px', delay: 50, interval: 100})
 sr.reveal('.skills__img', {delay: 400})
 
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
-
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
+
+/*SCROLL CAREER*/
+sr.reveal('.cd-timeline-block', {delay: 400})
+sr.reveal('.left', {origin:'left', delay: 400})
+sr.reveal('.right', {origin:'right', delay: 400})
