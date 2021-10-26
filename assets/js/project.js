006ff0d90a3b9f52e956a23bafa27eb5c1262436
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-10-26 21:33:53 +0200

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,83 +1,85 @@
-AOS.init();
-/* Project Cards */
+/*Research Details Table*/
 
-const projectcards = document.querySelector(".projectcards");
+const researchTable = document.querySelector(".main");
 
-// Array of object for projects
-const projects = [
-  {
-    title: "Notepy",
-    cardImage: "https://github.com/Mirko-r/Notepy/raw/main/notepy2-5.jpg",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "https://mirko-r.github.io/notepy/index.html",
-    Githublink: "https://github.com/Mirko-r/Notepy",
-  },
-  {
-    title: "Dotfiles",
-    cardImage: "https://camo.githubusercontent.com/d2a503d4c4b870701247e650d85aebf94a770482217428c8e15c1bd6a2b31c8a/68747470733a2f2f74682e62696e672e636f6d2f74682f69642f4f49502e6937656f5a346b4d4c7543654a4f78756c456d313367486144423f7069643d496d674465742672733d31",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "https://github.com/Mirko-r/dotfiles",
-  },
-  {
-    title: "Extractor",
-    cardImage: "https://github.com/Mirko-r/extractor/raw/main/Extrattor1.0.png",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "https://github.com/Mirko-r/extractor",
-  },
+const research = [{
+        title: "Notepy",
+        authors: "Mirko Rovere",
+        conferences: "Notepy is a full-featured Notepad Python app",
+        researchYr: 2021,
+        citebox: "https://github.com/Mirko-r/Notep",
+        image: "https://github.com/Mirko-r/Notepy/raw/main/notepy2-5.jpg",
+        abstract: "https://mirko-r.github.io/notepy/index.html",
+    },
+
+    {
+        title: "Dotfiles",
+        authors: "Mirko Rovere",
+        conferences: "Backup, Restore, Learn, Share",
+        researchYr: 2021,
+        citebox: "https://github.com/Mirko-r/extractor",
+        image: "https://camo.githubusercontent.com/d2a503d4c4b870701247e650d85aebf94a770482217428c8e15c1bd6a2b31c8a/68747470733a2f2f74682e62696e672e636f6d2f74682f69642f4f49502e6937656f5a346b4d4c7543654a4f78756c456d313367486144423f7069643d496d674465742672733d31",
+        abstract: "https://mirko-r.github.io/404.html",
+    },
+
+    {
+        title: "Extractor",
+        authors: "Mirko Rovere",
+        conferences: "Extractor is a simple Linux utility to extract one or more archives from the terminal",
+        researchYr: 2020,
+        citebox: "https://github.com/Mirko-r/extractor",
+        image: "https://github.com/Mirko-r/extractor/raw/main/Extrattor1.0.png",
+        abstract: "https://mirko-r.github.io/404.html",
+    }
+
 ];
+AOS.init();
+const fillData = () => {
+    let output = "";
+    research.forEach(
+        ({
+            image,
+            title,
+            authors,
+            conferences,
+            researchYr,
+            citebox,
+            abstract
+        }) =>
+        (output += `
+            <tr data-aos="zoom-in-left"> 
+                <td class="imgCol"><img src="${image}" class="rImg"></td>
+                <td class = "researchTitleName">
+                    <div>
+                        <span class="imgResponsive">
+                            <img src="${image}" class="imgRes">
+                        </span>
+                    </div>
+                    <a href="#0" class="paperTitle"> ${title} </a> 
+                    <div> ${authors} </div> <div class="rConferences"> ${conferences} 
+                        <div class="researchY">${researchYr}</div>
+                    </div>
+        
+                    <!--CITE BUTTON-->
+                    <div class="d-flex" style="margin-right:5%">
+                        
+                            <a href="${abstract}" style="color:white;">
+                            <button class="button button-accent button-small text-right button-abstract " type="button" >
+                            SITE
+                            </button>
+                            </a>
+                            &nbsp;&nbsp;&nbsp;
+                            <a href="${citebox}" style="color:white;">
+                            <button class="button button-accent button-small text-right button-abstract " type="button" >
+                            GITHUB
+                            </button>
+                            </a>
+                        
+                    </div>
+                </td>
+            </tr>`)
+    );
+    researchTable.innerHTML = output;
 
-// function for rendering project cards data
-const showCards = () => {
-  let output = "";
-  projects.forEach(
-    ({ title, cardImage, Previewlink, Githublink }) => {
-      (output += `       
-        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
-          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-            <div class="header">
-            </div>
-            <div class="data">
-              <div class="content">
-              <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
-                </div>
-            <ul class="menu-content"><br>
-                  <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
-                  <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>`
-      )
-    }
-  );
-  projectcards.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards);
-
-function myFunction() {
-  // Declare variables
-  var input, button, i, skillcard, card, title;
-  input = document.getElementById("myInput").value;
-  input = input.toUpperCase();
-  skillcard = document.getElementsByClassName("skill-card");
-  card = document.getElementsByClassName("card");
-  title = document.getElementsByClassName("title");
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < button.length; i++) {
-    if (
-      button[i].innerHTML.toUpperCase().includes(input) ||
-      title[i].innerHTML.toUpperCase().includes(input)
-    ) {
-      skillcard[i].style.display = "";
-      card[i].style.display = "";
-    } else {
-      skillcard[i].style.display = "none";
-      card[i].style.display = "none";
-    }
-  }
-}
+document.addEventListener("DOMContentLoaded", fillData);
