6a12cf47f0ee2b7808becc30968ebbe4e0ee08d3
Mirko-r <rovere04@gmail.com>
2026-01-23 18:10:50 +0100

    BugFixes, added astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mirko-r.github.io',
@@ -16,7 +18,7 @@ export default defineConfig({
     format: 'file',
   },
 
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
